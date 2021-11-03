7003d2ab3b7bd263f8d7a25ead5a8c3f23710323
mrAlishah <mralishprg@gmail.com>
2021-11-03 13:29:16 +0330

    06-01-Write2File

--- 06-Cuncurrency/01-BatchFiles/main.go
+++ 06-Cuncurrency/01-BatchFiles/main.go
@@ -11,12 +11,18 @@ import (
 
 func main() {
 	fld := os.Args[1]
+	outfile := os.Args[2]
 
 	//Read Folder files
 	err := filepath.Walk(fld, func(file string, filInfo os.FileInfo, err error) error {
 		if !filInfo.IsDir() {
+
 			md5, err := md5Generate(file)
 			fmt.Printf("MD5= %s , File= %s , Error=%v \n", md5, file, err)
+
+			if err := writeToFile(file, outfile, md5); err != nil {
+				panic(err)
+			}
 		}
 		return nil
 	})
@@ -43,4 +49,18 @@ func md5Generate(filename string) (string, error) {
 	md5 := string(hex.EncodeToString(checksum))
 	return md5, nil
 
+}
+
+//Write info to args[2] filename
+func writeToFile(filename, outfilename, md5sum string) error {
+	file, err := os.OpenFile(outfilename, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0755) //ModePerm FileMode = 0777 // Unix permission bits
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	file.WriteString(fmt.Sprintf("%s ; %s \n", md5sum, filename))
+
+	//https://stackoverflow.com/questions/10862375/when-to-flush-a-file-in-go
+	return file.Sync()
 }

a12b878e9d601691021a6c520316c81d4b2c2c53
mrAlishah <mralishprg@gmail.com>
2021-11-03 12:56:38 +0330

    06-01-Generate MD5

--- 06-Cuncurrency/01-BatchFiles/main.go
+++ 06-Cuncurrency/01-BatchFiles/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 )
@@ -10,9 +13,10 @@ func main() {
 	fld := os.Args[1]
 
 	//Read Folder files
-	err := filepath.Walk(fld, func(filename string, filInfo os.FileInfo, err error) error {
+	err := filepath.Walk(fld, func(file string, filInfo os.FileInfo, err error) error {
 		if !filInfo.IsDir() {
-			fmt.Println(filename)
+			md5, err := md5Generate(file)
+			fmt.Printf("MD5= %s , File= %s , Error=%v \n", md5, file, err)
 		}
 		return nil
 	})
@@ -20,4 +24,23 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+//Create MD5
+func md5Generate(filename string) (string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	hash := md5.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return "", err
+	}
+
+	checksum := hash.Sum(nil)
+	md5 := string(hex.EncodeToString(checksum))
+	return md5, nil
+
 }

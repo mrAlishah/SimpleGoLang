06429e3a0e561a92363e4c3399bcec2fa7c6c5ab
mrAlishah <mralishprg@gmail.com>
2021-11-08 17:17:12 +0330

    06-01-batch concurrent

--- 06-Cuncurrency/01-BatchFiles/main.go
+++ 06-Cuncurrency/01-BatchFiles/main.go
@@ -24,20 +24,28 @@ func main() {
 	writeDone := make(chan struct{})
 
 	go func() {
+		cache := make([][]string, 0)
 		for result := range librerian {
-			writeToFile(result[1], outfile, result[0], result[2])
+			cache = append(cache, result)
+
+			if len(cache) >= 1000 {
+				writeToFile(cache, outfile)
+				cache = nil
+			}
 		}
-		writeDone <- struct{}{} //if writedon==nil return main thread to rutin else end prog and exit main
+		if len(cache) > 0 {
+			writeToFile(cache, outfile)
+			cache = nil
+		}
+		writeDone <- struct{}{}
 	}()
 	//-----------------------------------------
 
 	wg := &sync.WaitGroup{}
-	//-----------------------------------------
 
 	//fan out
 	pipline := make(chan string)
 	createWorkers(10, pipline, librerian, wg)
-	//-----------------------------------------
 
 	go func() {
 		wg.Wait() //wait wg emtpty and worker finished
@@ -48,7 +56,6 @@ func main() {
 	//Read Folder files
 	err := filepath.Walk(fld, func(file string, filInfo os.FileInfo, err error) error {
 		if !filInfo.IsDir() {
-
 			//ِdistributed task between workers
 			fmt.Printf("- pipline <- file => %s \n", file)
 			pipline <- file
@@ -68,7 +75,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	//-----------------------------------------
+
 	<-writeDone
 }
 
@@ -92,15 +99,21 @@ func md5Generate(filename string) (string, error) {
 }
 
 //Write info to args[2] filename
-func writeToFile(filename, outfilename, md5sum, workerId string) error {
+//func writeToFile(filename, outfilename, md5sum, workerId string) error {
+func writeToFile(inputs [][]string, outfilename string) error {
 	file, err := os.OpenFile(outfilename, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0755) //ModePerm FileMode = 0777 // Unix permission bits
 	if err != nil {
 		return err
 	}
 	defer file.Close()
 
-	file.WriteString(fmt.Sprintf("%s ; %s \n", md5sum, filename))
-	fmt.Printf("-> Worker #%s Write=> %s \n", workerId, filename)
+	//file.WriteString(fmt.Sprintf("%s ; %s \n", md5sum, filename))
+	//fmt.Printf("-> Worker #%s Write=> %s \n", workerId, filename)
+	for _,input := range inputs{
+		file.WriteString(fmt.Sprintf("%s ; %s \n",input[0], input[1]))
+	}
+
+
 	//https://stackoverflow.com/questions/10862375/when-to-flush-a-file-in-go
 	return file.Sync()
 }
